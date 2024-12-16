9bd05e81462e4c0c2cc2bf51fc9ee8aeaf6b8cbf
Hatzilu <itamar246@gmail.com>
2024-12-16 22:15:53 +0200

    Add shadCN config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
-import colors from 'tailwindcss/colors'
+const animate = require('tailwindcss-animate')
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+  ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     colors: {
       ...colors,
       tresBlue: {
@@ -18,6 +33,23 @@ export default {
         1100: '#1F1F1F',
       },
     },
+
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
-  plugins: [],
+  plugins: [animate],
 }
