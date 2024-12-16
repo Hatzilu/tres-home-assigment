ed3cddf927536d91b08a8c835b82091b7d7f042e
Hatzilu <itamar246@gmail.com>
2024-12-16 21:04:34 +0200

    make it so accordion can be controlled from outside

--- src/components/Accordion/Accordion.vue
+++ src/components/Accordion/Accordion.vue
@@ -1,9 +1,11 @@
+<script setup lang="ts">
+const props = defineProps(['open'])
+</script>
 <template>
-  <article>
-    <details
-      class="bg-white border border-tresBlue-300 rounded-[4px] open:rounded-bl-none open:rounded-br-none"
-    >
-      <slot />
-    </details>
-  </article>
+  <details
+    class="bg-white border border-tresBlue-300 rounded-[4px] open:rounded-bl-none open:rounded-br-none"
+    :open="props.open ? 'true' : undefined"
+  >
+    <slot />
+  </details>
 </template>
